a032be4f2e23d4589ca0896992ebbe3e08e94f05
qixqi <zhengxiang4056@gmail.com>
2021-01-10 14:50:11 +0800

    Github IP 爬取

---
+++ github-spider/run.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+for i in open("github-spider/domain.txt"):
+    url = "http://ip.chinaz.com/" + i.strip()
+    resp = requests.get(url)
+    soup=BeautifulSoup(resp.text)
+    x=soup.find(class_="IcpMain02")
+    x=x.find_all("span",class_="Whwtdhalf")
+    print(x[5].string.strip(),i.strip())
